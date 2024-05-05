e82dc8235e317652c9f47f44797e26be1c140382
minhdn0404 <minhwhisky700@gmail.com>
2024-05-05 14:59:37 +0700

    temp register

--- app/src/main/java/com/example/btl2/MainActivity.java
+++ app/src/main/java/com/example/btl2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.btl2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button btnLogout;
+    TextView textView;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.logOut);
+        textView = findViewById(R.id.userDetails);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            textView.setText(user.getDisplayName());
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/example/btl2/Register.java
+++ app/src/main/java/com/example/btl2/Register.java
@@ -1,6 +1,14 @@
 package com.example.btl2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +16,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.btl2.api.FirebaseAPI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Register extends AppCompatActivity {
+    EditText editTextName, editTextPhone, editTextEmail, editTextPassword;
+    Button btnRegister;
+    TextView textViewLogin;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +52,68 @@ public class Register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextName = findViewById(R.id.editText_Name);
+        editTextEmail = findViewById(R.id.editText_Email);
+        editTextPassword = findViewById(R.id.editText_Password);
+        editTextPhone = findViewById(R.id.editText_Phone);
+        btnRegister = findViewById(R.id.button_Register);
+        textViewLogin = findViewById(R.id.text_Login);
+        mAuth = FirebaseAuth.getInstance();
+
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, username, phone;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+                username = String.valueOf(editTextName.getText());
+                phone = String.valueOf(editTextPhone.getText());
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("Users");
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(Register.this, "Enter username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(phone)) {
+                    Toast.makeText(Register.this, "Enter phone", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(Register.this, "Password must be >= 6", Toast.LENGTH_SHORT).show();
+                }
+
+                UserClass userClass = new UserClass(username, phone, email, password);
+                reference.child(username).setValue(userClass);
+
+                Toast.makeText(Register.this, "Register Success", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
 }
