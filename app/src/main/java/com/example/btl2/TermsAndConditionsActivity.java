c362d14388f05caa45cee845d2925366724c7a31
MinhHieutn <minhhieu23503@gmail.com>
2024-05-20 22:58:15 +0700

    UI của terms

--- app/src/main/java/com/example/btl2/TermsAndConditionsActivity.java
+++ app/src/main/java/com/example/btl2/TermsAndConditionsActivity.java
@@ -1,6 +1,10 @@
 package com.example.btl2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TermsAndConditionsActivity extends AppCompatActivity {
+import com.example.btl2.fragment.HomeFragment;
 
+public class TermsAndConditionsActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_terms_and_conditions);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+        button = (Button) findViewById(R.id.btBack);
     }
 }
