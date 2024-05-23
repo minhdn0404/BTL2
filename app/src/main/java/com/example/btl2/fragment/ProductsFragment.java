9ee145a13d4754f6f3a30561189bbfff04d2147d
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2024-05-23 10:27:34 +0700

    Show My Product

--- app/src/main/java/com/example/btl2/fragment/ProductsFragment.java
+++ app/src/main/java/com/example/btl2/fragment/ProductsFragment.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.btl2.ProductListAdapter;
 import com.example.btl2.R;
 import com.example.btl2.databinding.FragmentProductsBinding;
 import com.example.btl2.models.Product;
 import com.example.btl2.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsFragment extends Fragment {
+    RecyclerView productRecycler;
+    ProductListAdapter productListAdapter2;
     private FragmentProductsBinding binding;
     List<Product> productList;
     User user;
@@ -32,6 +38,7 @@ public class ProductsFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentProductsBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
+        productRecycler = binding.productRecycler;
         return binding.getRoot();
     }
 
@@ -39,6 +46,18 @@ public class ProductsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        List<Product> filteredProductlist = new ArrayList<>();
+        for (Product product : productList) {
+            if (product.getOwner().equals(user.getId())) {
+                filteredProductlist.add(product);
+            }
+        }
+
+        productListAdapter2 = new ProductListAdapter(getActivity(), filteredProductlist);
+        productRecycler.setAdapter(productListAdapter2);
+        productRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        productRecycler.setHasFixedSize(true);
+        productRecycler.setNestedScrollingEnabled(false);
         // TODO: Hiện ra những sản phẩm do mình tạo ra
     }
 }
